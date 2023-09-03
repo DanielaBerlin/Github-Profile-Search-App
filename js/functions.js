c99ffe7cefff700e62664447d63d76da9cc1a4d4
Daniela <danielacardona2203@gmail.com>
2023-09-03 19:33:13 +0200

    Add errorMessage and displayData functions

--- js/functions.js
+++ js/functions.js
@@ -5,23 +5,39 @@ export async function getUser(username) {
   const response = await fetch(v.apiURL + username);
   const data = await response.json();
 
-  // console.log(data);
+  console.log(data);
   if (!response.ok) {
     errorMessage('User not found, try another');
   } else {
     displayData(data);
-    getRepos(username);
+    // getRepos(username);
   }
 }
 
 export function errorMessage(msg) {
   v.profile.innerHTML = '';
-  document.querySelector('.hide').style.displayData = 'none';
+  document.querySelector('.hide').style.display = 'none';
   return (v.repos.innerHTML = `<p class="alert alert-danger">${msg}</p>`);
 }
 
-export displayData(user) {
-    const html = `
-    
-    `
+function displayData(user) {
+  const html = `
+    <img
+                src="${user.avatar_url}"
+                alt="${user.name}"
+                class="img-thumbnail rounded-circle"
+              />
+              <h2>${user.name}</h2>
+              <p>${user.login}/p>
+              <div class="d-grid">
+                <a href="https://github.com/${user.login}" target="_blank" rel="noopener" class="btn btn-outline-secondary">View Profile</a>
+              </div>
+              <p class="pt-2">
+                <span>${user.followers}Followers</span>
+                <span>${user.following} Following</span>
+              </p>
+              <p>${user.public_repos}Repos</p>
+              <p><i class="fas fa-map-marker-alt"></i>${user.location}</p>
+    `;
+  v.profile.innerHTML = html;
 }
